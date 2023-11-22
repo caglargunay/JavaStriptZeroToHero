2ffd05825be9dfd43d66eb9ca5653db12b90f4ad
caglargunay <caglarguenay@gmail.com>
2023-11-22 21:43:34 +0100

    task 6 completed

---
+++ P6.js
@@ -0,0 +1,16 @@
+//Print the odd numbers from an integer array;
+let arr = [1,2,3,4,5,6,7,8,9,12,114,41];
+
+//first solution
+
+for(each of arr){
+    if(each %2 !=0) console.log(each);
+}
+
+//second solution
+console.log("second solution");
+arr.filter(i => i % 2 == 1).forEach(i=>console.log(i));
+
+//third solution
+console.log("third solution");
+arr.forEach(i=> {if(i % 2 == 1) console.log (i)})
